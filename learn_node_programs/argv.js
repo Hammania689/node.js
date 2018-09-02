c95b092331929830085fd9b9324371fbb7e94aa9
Hammania689 <Hameedabdulrashid@gmail.com>
2018-09-02 01:39:20 -0500

    Two down. Nine to go ;)

--- learn_node_programs/argv.js
+++ learn_node_programs/argv.js
@@ -1,19 +1,29 @@
-console.log(process.argv);
-
 // Get input (after file)
 input = process.argv.slice(2);
-
-// Print conversion for sanity check
-console.log(input);
+// console.log(process.argv);
 
 
-
-let sumResult = ((x) => {
-  let sum = 0;
-  sum += Number(x);
-  return sum;
+let convertStringToNum = ((arr)=> {
+    let intArr = [];
+    for (let i in arr){
+        intArr.push(Number(arr[i]));
+    }
+    // Print conversion for sanity check
+    // console.log(intArr);
+    return intArr;
 });
 
-console.log(sumResult(input));
-//console.log(sumResult(input));
+// Reassign input
+input = convertStringToNum(input);
+
+// Func to add all elements
+let sumOfAllElements = ((intArr) => {
+    let sum = 0;
+    for(let i in intArr)
+        sum += intArr[i];
+    return sum;
+});
+
+// console.log("Hello from remote laptop");
+console.log(sumOfAllElements(input));
 
